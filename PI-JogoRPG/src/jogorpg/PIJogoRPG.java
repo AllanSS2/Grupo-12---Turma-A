dcfd968ded240888cc7be1a0868f76626b51a118
yJuanz <juanpereira9542@gmail.com>
2024-05-03 20:30:47 -0300

    TESTE 2

--- PI-JogoRPG/src/jogorpg/PIJogoRPG.java
+++ PI-JogoRPG/src/jogorpg/PIJogoRPG.java
@@ -8,6 +8,7 @@ public class PIJogoRPG {
     public static void main(String[] args) {
         System.out.println("Teste primeiro commit");
         System.out.println("Teste Juan");
+        System.out.println("teste3");
     }
     
 }
