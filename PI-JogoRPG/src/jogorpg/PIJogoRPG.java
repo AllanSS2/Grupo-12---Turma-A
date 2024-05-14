deefca4dec88454ccb54f76073a1d4d9fde94bb2
IRAN VIEIRA DE ARAUJO <iran.varaujo@senacsp.edu.br>
2024-05-13 22:22:31 -0300

    inicio

--- PI-JogoRPG/src/jogorpg/PIJogoRPG.java
+++ PI-JogoRPG/src/jogorpg/PIJogoRPG.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public class PIJogoRPG {
 
 
-        static int temp_dialog = 20, temp_narrativa = 75, temp_transicao = 40;
+        static int temp_dialog = 50, temp_narrativa = 75, temp_transicao = 40;
 
 
 
@@ -47,33 +47,74 @@ public class PIJogoRPG {
                     creditos();
                 }
                 else if (escolha_opcao == 4){
-                    Escreva("Não seja fraco, volte e conquiste o mundo!", TimeUnit.MILLISECONDS, temp_dialog);
+                    Escreva("\nNão seja fraco, volte e conquiste o mundo!\n", TimeUnit.MILLISECONDS, temp_dialog);
                     break;
                 }
                 else{
-                    Escreva("hm... essa opção não conheço, mas escolha alguma correspondente:", TimeUnit.MILLISECONDS, temp_dialog);
+                    Escreva("\nhm... essa opção não conheço, mas escolha alguma correspondente:\n", TimeUnit.MILLISECONDS, temp_dialog);
                 }
 
-            }while(escolha_opcao !=3 );
+            }while(escolha_opcao != 1 && escolha_opcao != 2 && escolha_opcao != 3 && escolha_opcao != 4 );
         }
 
 
         static void jogo() throws Exception{
             Scanner entrada = new Scanner(System.in);
-            Escreva("Narrador: Espera... Quem é você? Não vou tentar adivinhar...\n\n", TimeUnit.MILLISECONDS, temp_dialog);
-            Escreva("Prefiro te chamar de Ace, um ótimo nome para um guerreiro como você!\n\n", TimeUnit.MILLISECONDS, temp_dialog);
-            Escreva("Narrador: Mas não se deixe enganar só pelo elogio... os desafios serão grandes! Vai precisar de culhão!\n\n",TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("Narrador: Espera... Quem é você? Não, vou tentar adivinhar...\n\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("Devo chamá-lo de Ace? hmmm... Jovem Ace, um ótimo nome para um guerreiro como você!\n\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("Narrador: Mas não se deixe enganar só pelo elogio... os desafios serão grandes! você irá precisar de coragem!\n\n",TimeUnit.MILLISECONDS, temp_dialog);
             Escreva("Narrador: Não fique com medo, um guerreiro não desiste fácil!\n\n", TimeUnit.MILLISECONDS, temp_dialog);
             Escreva("Se sente preparado?\n1 - Porque eu teria medo?\n2 - Pega leve\n", TimeUnit.MILLISECONDS, temp_dialog);
             int escolha_opcao = entrada.nextInt();
 
             switch(escolha_opcao){
                 case 1:
+                    Escreva("\nNarrador: Hahaha, jovem e destemido, não esperava menos de um grande guerreiro!\n\n", TimeUnit.MILLISECONDS, temp_dialog);
                     break;
                 case 2:
-                    menu();
+                    Escreva("\nNarrador: Hahaha, não se preocupe meu jovem, você irá gostar...\n", TimeUnit.MILLISECONDS, temp_dialog);
                     break;
             }
+            Escreva("\nNarrador: Aqui se inicia sua jornada, desejo-lhe boa sorte!!!!\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\nPai: Ei Ace, acorde seu preguiçoso!!!!\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\nAce: argh... Deixe eu continuar descansando pai...\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\nPai: ACEEEEEE, por que você é tão preguiçoso???\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\nPai: Na sua idade eu já viajava cidades!!!\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\nVocê vai querer levantar?\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\n1- Sim | 2- Não\n", TimeUnit.MILLISECONDS, temp_dialog);
+            int escolha = entrada.nextInt();
+            switch (escolha) {
+                case 1:
+                Escreva("\nAce: argh... que saco, eu vou me lavantar\n", TimeUnit.MILLISECONDS, temp_dialog);    
+                    break;
+                
+                case 2:
+                Escreva("\nMãe: ACEEEEEE, EU VOU TE BATEEEEEEEEER!!!\n", TimeUnit.MILLISECONDS, temp_dialog);
+                Escreva("\nNarrador: Após Ace ter sua coluna fraturada, ele entendeu que tinha que levantar hahaha\n", TimeUnit.MILLISECONDS, temp_dialog);
+                default:
+                    break;
+            }
+            Escreva("\nNarrador: Depois de ter um grande bom dia de seus pais, tomar seu café da manhã, Ace foi ajudar seu pai em seu trabalho\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\nPai: Ace, venha ajudar seu velho a carregar uns pesos, eu estou velho...\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\nPai: E também quero te contar uma grande história\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\nVocê vai querer ouvir essa história misteriosa...?\n", TimeUnit.MILLISECONDS, temp_dialog);
+            Escreva("\n1- Sim | 2- Não\n", TimeUnit.MILLISECONDS, temp_dialog);
+            int escolha2 = entrada.nextInt();
+            switch (escolha2) {
+                case 1:
+                Escreva("\nAce: Estou indo pai, espero que essa história seja interessante... :p\n ", TimeUnit.MILLISECONDS, temp_dialog);
+                    break;
+                
+                case 2:
+                Escreva("\nAce: Eu te ajudo velhote, mas não estou muito interessado nessa história...\n", TimeUnit.MILLISECONDS, temp_dialog);
+                Escreva("\nPai: Hahaha, eu tenho certeza que você vai achar-lá muito interessante\n", TimeUnit.MILLISECONDS, temp_dialog);
+                Escreva("\nAce: Hmpf...\n", TimeUnit.MILLISECONDS, temp_dialog);
+                default:
+                    break;
+            }
+
+
+
         }
 
         
